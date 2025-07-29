f415ae6adcf348c4a7d778ffc7ec3fa15cfeb086
pearl-174 <115370971+pearl174@users.noreply.github.com>
2025-07-29 18:43:43 +0530

    Shift home code to function. Return parent div from function. Create display function in index. Append parentDiv to contentDiv and run display function.

--- src/home.js
+++ src/home.js
@@ -2,31 +2,36 @@ import restaurantBannerImg from "./wanmin-restaurant.jpg";
 import chefMao from "./chefmao.png";
 import xiangling from "./xiangling.png";
 
-const contentDiv = document.querySelector("#content");
-const restaurantBanner = document.createElement("img");
-restaurantBanner.src = restaurantBannerImg;
-restaurantBanner.classList.add("restaurant-banner");
-contentDiv.appendChild(restaurantBanner);
+function home() {
+    const parentDiv = document.createElement("div");
+    const restaurantBanner = document.createElement("img");
+    restaurantBanner.src = restaurantBannerImg;
+    restaurantBanner.classList.add("restaurant-banner");
+    parentDiv.appendChild(restaurantBanner);
 
-const welcomeDiv = document.createElement("div");
-welcomeDiv.classList.add("welcome-text");
-welcomeDiv.innerHTML = `<h1>WELCOME TO WANMIN RESTAURANT</h1>
-                        <p>A new twist<br>
-                        On an old classic<br>   
-                        Wanmin Restaurant welcomes you!</p>`
-contentDiv.appendChild(welcomeDiv);
+    const welcomeDiv = document.createElement("div");
+    welcomeDiv.classList.add("welcome-text");
+    welcomeDiv.innerHTML = `<h1>WELCOME TO WANMIN RESTAURANT</h1>
+                            <p>A new twist<br>
+                            On an old classic<br>   
+                            Wanmin Restaurant welcomes you!</p>`
+    parentDiv.appendChild(welcomeDiv);
 
-const chefSectionDiv = document.createElement("div");
-chefSectionDiv.classList.add("chefs-section");
-chefSectionDiv.innerHTML = `<div class="chef-card">
-                            <img src=${xiangling} alt="Xiangling">
-                            <h3>Xiangling</h3>
-                            <p>The Pyro prodigy behind every fiery dish. Passionate, fearless, and just a little too comfortable with Jueyun chilis.</p>
-                            </div>
+    const chefSectionDiv = document.createElement("div");
+    chefSectionDiv.classList.add("chefs-section");
+    chefSectionDiv.innerHTML = `<div class="chef-card">
+                                <img src=${xiangling} alt="Xiangling">
+                                <h3>Xiangling</h3>
+                                <p>The Pyro prodigy behind every fiery dish. Passionate, fearless, and just a little too comfortable with Jueyun chilis.</p>
+                                </div>
 
-                            <div class="chef-card">
-                            <img src=${chefMao} alt="Chef Mao">
-                            <h3>Chef Mao</h3>
-                            <p>The founder and backbone of Wanmin. Calm, collected, and master of traditional Liyue recipes — Guoba’s personal favorite.</p>
-                            </div>`
-contentDiv.appendChild(chefSectionDiv);
+                                <div class="chef-card">
+                                <img src=${chefMao} alt="Chef Mao">
+                                <h3>Chef Mao</h3>
+                                <p>The founder and backbone of Wanmin. Calm, collected, and master of traditional Liyue recipes — Guoba’s personal favorite.</p>
+                                </div>`
+    parentDiv.appendChild(chefSectionDiv);
+    return parentDiv;
+}
+
+export default home;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import "./styles.css";
-import "./home.js";
+import home from "./home.js";
+
+function display() {
+    const contentDiv = document.querySelector("#content");
+    contentDiv.append(home());
+}
+
+display();
