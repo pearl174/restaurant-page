f683f98d7a51fdab74a963e15d83d76cc3799c8c
pearl-174 <115370971+pearl174@users.noreply.github.com>
2025-07-29 22:35:53 +0530

    Add about page in template.html.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 import homeContainer from "./home/home.js";
 import menuContainer from "./menu/menu.js";
+import "./aboutStyles.css";
+import "./styles.css";
 
 const home = document.querySelector("#home");
 const menu = document.querySelector("#menu");
 const about = document.querySelector("#about");
 const contentDiv = document.querySelector("#content");
 
-home.addEventListener("click", () => {
-    contentDiv.innerHTML = "";
-    contentDiv.append(homeContainer());    
-})
-
-menu.addEventListener("click", () => {
-    contentDiv.innerHTML = "";
-    contentDiv.append(menuContainer());
-})
-
-// about.addEventListener("click", () => {
+// home.addEventListener("click", () => {
 //     contentDiv.innerHTML = "";
-//     contentDiv.append(aboutContainer());
+//     contentDiv.append(homeContainer());    
 // })
 
-contentDiv.append(homeContainer());
+// menu.addEventListener("click", () => {
+//     contentDiv.innerHTML = "";
+//     contentDiv.append(menuContainer());
+// })
+
+// // about.addEventListener("click", () => {
+// //     contentDiv.innerHTML = "";
+// //     contentDiv.append(aboutContainer());
+// // })
+
+// contentDiv.append(homeContainer());
