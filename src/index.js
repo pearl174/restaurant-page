32245aefd8b32333a8909f501ea92b79bd494262
pearl-174 <115370971+pearl174@users.noreply.github.com>
2025-07-29 21:08:50 +0530

    Add button navigation to menu and home.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-import home from "./home/home.js";
-import "./menuStyles.css";
+import homeContainer from "./home/home.js";
+import menuContainer from "./menu/menu.js";
 
-function display() {
-    const contentDiv = document.querySelector("#content");
-    contentDiv.append(home());
-}
+const home = document.querySelector("#home");
+const menu = document.querySelector("#menu");
+const about = document.querySelector("#about");
+const contentDiv = document.querySelector("#content");
 
-// display();
+home.addEventListener("click", () => {
+    contentDiv.innerHTML = "";
+    contentDiv.append(homeContainer());    
+})
+
+menu.addEventListener("click", () => {
+    contentDiv.innerHTML = "";
+    contentDiv.append(menuContainer());
+})
+
+// about.addEventListener("click", () => {
+//     contentDiv.innerHTML = "";
+//     contentDiv.append(aboutContainer());
+// })
+
+contentDiv.append(homeContainer());
